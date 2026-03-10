4d8fff2622805fa2d6db140b85248c15ce9e5dab
Prathyusha Vemula <prathyusha.vemula@hpe.com>
2026-03-10 16:54:43 -0700

    MORPH-7952: Added example for system details custom tabs

--- morpheus-system-example/src/main/groovy/com/morpheusdata/system/example/SystemExamplePlugin.groovy
+++ morpheus-system-example/src/main/groovy/com/morpheusdata/system/example/SystemExamplePlugin.groovy
@@ -5,7 +5,7 @@ import com.morpheusdata.model.Icon
 import com.morpheusdata.system.example.datasets.StorageTypeDatasetProvider
 import com.morpheusdata.system.example.workflow.ArcusSystemConfigurationWorkflowProvider
 import com.morpheusdata.system.example.wizards.*
-
+import com.morpheusdata.system.example.tabs.*
 /**
  * Example System Provider Plugin demonstrating configuration workflow workflow
  * Based on the mip-arcus-10 system configuration screenshot
@@ -19,29 +19,38 @@ class SystemExamplePlugin extends Plugin {
 
     @Override
     void initialize() {
-        this.setName("System Example Plugin")
-        
-        // Register input type library for custom components
-        this.registerProvider(new ArcusInputTypeLibraryProvider(this, morpheus))
-        
-        // Register dataset providers
-        this.registerProvider(new StorageTypeDatasetProvider(this, morpheus))
-        
-        // Register system provider
-        this.registerProvider(new ArcusSystemProvider(this, morpheus))
-        
-        // Register configuration workflow provider
-        this.registerProvider(new ArcusSystemConfigurationWorkflowProvider(this, morpheus))
-        
-        // Register wizard providers
-        this.registerProvider(new SystemConfigWizardProvider(this, morpheus))
-        this.registerProvider(new SwitchConfigWizardProvider(this, morpheus))
-        this.registerProvider(new HostConfigWizardProvider(this, morpheus))
-        this.registerProvider(new StorageConfigWizardProvider(this, morpheus))
-        this.registerProvider(new DataNetworkConfigWizardProvider(this, morpheus))
-        this.registerProvider(new ClusterConfigWizardProvider(this, morpheus))
-        this.registerProvider(new PrechecksWizardProvider(this, morpheus))
-    }
+         this.setName("System Example Plugin")
+         try {
+            // PHASE 1: Register core providers first
+            this.registerProvider(new ArcusInputTypeLibraryProvider(this, morpheus))
+            this.registerProvider(new StorageTypeDatasetProvider(this, morpheus))
+           
+            // PHASE 2: Register wizard providers (before workflow that depends on them)
+            this.registerProvider(new SystemConfigWizardProvider(this, morpheus))
+            this.registerProvider(new SwitchConfigWizardProvider(this, morpheus))
+            this.registerProvider(new HostConfigWizardProvider(this, morpheus))
+            this.registerProvider(new StorageConfigWizardProvider(this, morpheus))
+            this.registerProvider(new DataNetworkConfigWizardProvider(this, morpheus))
+            this.registerProvider(new ClusterConfigWizardProvider(this, morpheus))
+            this.registerProvider(new PrechecksWizardProvider(this, morpheus))
+
+
+            // PHASE 3: Register configuration workflow (depends on wizards)
+            this.registerProvider(new ArcusSystemConfigurationWorkflowProvider(this, morpheus))
+          
+            
+            // PHASE 4: Register system provider (depends on workflow)
+             this.registerProvider(new ArcusSystemProvider(this, morpheus))
+
+             
+            // PHASE 5: Register tab providers (UI components)
+            this.registerProvider(new SystemExampleCustomSwitchesTabProvider(this, morpheus))
+            this.registerProvider(new SystemExampleCustomSummaryTabProvider(this, morpheus))
+            } catch (Exception e) {
+                 e.printStackTrace()
+                 throw e
+            }    
+     }
 
     @Override
     void onDestroy() {
---
+++ morpheus-system-example/src/main/groovy/com/morpheusdata/system/example/tabs/SystemExampleCustomSummaryTabProvider.groovy
@@ -0,0 +1,43 @@
+package com.morpheusdata.system.example
+
+import com.morpheusdata.core.providers.AbstractSystemTabProvider
+import com.morpheusdata.core.MorpheusContext
+import com.morpheusdata.core.Plugin
+import com.morpheusdata.model.Account
+import com.morpheusdata.model.User
+import com.morpheusdata.views.HTMLResponse
+import com.morpheusdata.views.ViewModel
+
+import com.morpheusdata.model.system.System
+
+class SystemExampleCustomSummaryTabProvider extends AbstractSystemTabProvider {
+
+	Plugin plugin
+	MorpheusContext morpheus
+
+	String code = "arcus-system-example-tab"
+	String name = "Summary Tab"
+
+	@Override
+	String getCode() { code }
+
+	@Override
+	String getName() { name }
+
+	SystemExampleCustomSummaryTabProvider(Plugin plugin, MorpheusContext morpheus) {
+		this.plugin = plugin
+		this.morpheus = morpheus
+	}
+
+	@Override
+	Boolean show(System system, User user, Account account) {
+		return true
+	}
+
+	@Override
+	HTMLResponse renderTemplate(System system) {
+		ViewModel<System> model = new ViewModel<>()
+		model.object = system
+		return getRenderer().renderTemplate("hbs/tabs/summaryTab", model)
+	}
+}
